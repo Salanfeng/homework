46f16b871bb5c821ed7bf199d2bb94b2719275be
飒岚枫 <2309411165@qq.com>
2023-04-06 21:24:45 +0800

    going on

--- 大作业/20230328151329105/simtool.c
+++ 大作业/20230328151329105/simtool.c
@@ -1,23 +1,175 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#define MAX 500
+char stopwords[500][15];
+int n = 0,Stopnum=0;
 
-char stopwords[330][15];
-
-int mainint argc,char* argv[])
+void insert(char *);
+int FindStop(char *);
+int ReadStop(FILE *);
+void ReadArticle(FILE *);
+struct Word
 {
-    FILE *article, *stop, *hash, *sample;
-    stop=fopen("stopwords.txt","r");
-    //read the stopwords
-    int i = 0;
-    while(fscanf(stop,"%s",stopwords[i])!=EOF)
+    char word[MAX];
+    int count;
+    struct Word *next;
+} WordNode;
+
+struct Word *head = NULL;
+struct Word *p;
+
+
+void insert(char *word)
+{
+    if (n == 0)
+    {
+        head = (struct Word *)malloc(sizeof(struct Word));
+        head->next = (struct Word *)malloc(sizeof(struct Word));
+        strcpy(head->next->word, word);
+        head->next->count = 1;
+        head->next->next = NULL;
+        n++;
+        return;
+    }
+    else
+    {
+        p = head;
+        for (int i = 0; i < n; i++)
+        {
+            if (strcmp(p->next->word, word) == 0)
+            {
+                p->next->count++;
+                return;
+            }
+            else if (i == n - 1)
+            {
+                struct Word *temp = (struct Word *)malloc(sizeof(struct Word));
+                strcpy(temp->word, word);
+                temp->count = 1;
+                if (strcmp(p->next->word, word) < 0)
+                {
+                    temp->next = NULL;
+                    p->next->next = temp;
+                }
+                else
+                {
+                    temp->next = p->next;
+                    p->next = temp;
+                }
+                n++;
+                return;
+            }
+            else if (strcmp(p->next->word, word) < 0)
+            {
+                p = p->next;
+            }
+            else if (strcmp(p->next->word, word) > 0)
+            {
+                struct Word *temp = (struct Word *)malloc(sizeof(struct Word));
+                strcpy(temp->word, word);
+                temp->count = 1;
+                temp->next = p->next;
+                p->next = temp;
+                n++;
+                return;
+            }
+        }
+    }
+}
+int ReadStop(FILE *stop)
+{
+    stop = fopen("stopwords.txt", "r");
+    if (stop == NULL)
+    {
+        printf("can not open stop file\n");
+        return 1;
+    }
+    
+    int i = 0, Stopnum = 0;
+    while (fscanf(stop, "%s", stopwords[i]) != EOF)
     {
         i++;
     }
-    fclose(stop);    
-    //read the article
-    article=fopen("article.txt","r");
+    Stopnum = i;
+    fclose(stop);
+    return Stopnum;
+}
+int FindStop(char *word)
+{
+    int head = 0,num=Stopnum;
+    while (head <= num)
+    {
+        if (strcmp(word, stopwords[head + (num - head) / 2]) == 0)
+        {
+            return 1;
+        }
+        else if (strcmp(word, stopwords[head + (num - head) / 2]) > 0)
+        {
+            head = head + (num - head) / 2 + 1;
+        }
+        else
+        {
+            num = head + (num - head) / 2 - 1;
+        }
+    }
+    return 0;
+}
 
+void ReadArticle(FILE *article)
+{
+    article = fopen("article.txt", "r");
+    if (article == NULL)
+    {
+        printf("can not open article file\n");
+        return 1;
+    }
+    char c;
+    c = fgetc(article);
+    int pos = 0;
+    int flag = 1;
+    char word[MAX];
+    while (c != EOF)
+    {
+        if (isalpha(c))
+        {
+            flag = 0;
+            c = tolower(c);
+            word[pos] = c;
+            pos++;
+        }
+        else
+        {
+            if (flag != 1)
+            {
+                word[pos] = '\0';
+                if (FindStop(word) == 0)
+                {
+                    insert(word);
+                }
+            }
+            flag = 1;
+            pos = 0;
+        }
+        c = fgetc(article);
+    }
+    fclose(article);
+}
 
+int main(int argc, char *argv[])
+{
+    int N, M;
+    N = atoi(argv[1]);
+    M = atoi(argv[2]);
+
+    FILE *article, *stop, *hash, *sample;
+    
+    // read the stopwords
+    Stopnum=ReadStop(stop);
+    
+    // read the article
+    ReadArticle(article);
+    
     return 0;
 }
--- 大作业/20230328151329105/test.c
+++ 大作业/20230328151329105/test.c
@@ -1,41 +1,125 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-char hashwords[500][15];
-
-int main(int argc,char* argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX 500
+int n = 0,len=0;
+char MW[MAX];
+void insert(char *);
+void ReadArticle(FILE *);
+struct Word
 {
-    int N, M;
-    N = atoi(argv[1]);
-    M = atoi(argv[2]);
-    FILE  *stop;
-    stop=fopen("stopwords.txt","r");
-    //read the stopwords with hash table
-    int i = 0,sum=0;
-    char temp[15];
-    while(fscanf(stop,"%s",temp)!=EOF)
+    char word[MAX];
+    int count;
+    struct Word *next;
+} WordNode;
+
+struct Word *head = NULL;
+struct Word *p;
+void ReadArticle(FILE *article)
+{
+    article = fopen("article.txt", "r");
+    if (article == NULL)
     {
-        int j = 0;
-        int hash = 0;
-        while(temp[j]!='\0')
-        {
-            hash = hash + temp[j]*temp[j];
-            j++;
-        }
-        hash = hash % 500;
-        strcpy(hashwords[hash],temp);
-        i++;
+        printf("can not open article file\n");
+        return ;
     }
-    fclose(stop);
-    int hsum = 0;
-    for (int i = 0; i < 500; i++)
+    char c;
+    c = fgetc(article);
+    int pos = 0;
+    int flag = 1;
+    char word[MAX];
+    while (c != EOF)
     {
-        if(hashwords[i][0]!='\0')
+        if (isalpha(c))
         {
-            hsum++;
+            flag = 0;
+            c = tolower(c);
+            word[pos] = c;
+            pos++;
+        }
+        else
+        {
+            if (flag != 1)
+            {
+                word[pos] = '\0';
+                //insert(word);
+                if(len<strlen(word))
+                {
+                    len=strlen(word);
+                    strcpy(MW, word);
+                }
+            }
+            flag = 1;
+            pos = 0;
+        }
+        c = fgetc(article);
+    }
+    fclose(article);
+}
+
+void insert(char *word)
+{
+    if (n == 0)
+    {
+        head = (struct Word *)malloc(sizeof(struct Word));
+        head->next = (struct Word *)malloc(sizeof(struct Word));
+        strcpy(head->next->word, word);
+        head->next->count = 1;
+        head->next->next = NULL;
+        n++;
+        return;
+    }
+    else
+    {
+        p = head;
+        for (int i = 0; i < n; i++)
+        {
+            if (strcmp(p->next->word, word) == 0)
+            {
+                p->next->count++;
+                return;
+            }
+            else if (i == n - 1)
+            {
+                struct Word *temp = (struct Word *)malloc(sizeof(struct Word));
+                strcpy(temp->word, word);
+                temp->count = 1;
+                if (strcmp(p->next->word, word) < 0)
+                {
+                    temp->next = NULL;
+                    p->next->next = temp;
+                }
+                else
+                {
+                    temp->next = p->next;
+                    p->next = temp;
+                }
+                n++;
+                return;
+            }
+            else if (strcmp(p->next->word, word) < 0)
+            {
+                p = p->next;
+            }
+            else if (strcmp(p->next->word, word) > 0)
+            {
+                struct Word *temp = (struct Word *)malloc(sizeof(struct Word));
+                strcpy(temp->word, word);
+                temp->count = 1;
+                temp->next = p->next;
+                p->next = temp;
+                n++;
+                return;
+            }
         }
     }
-    printf("%d", hsum);
+}
+
+int main()
+{
+    FILE *article;
+    ReadArticle(article);
+    printf("%d\n%s",len,MW);
     return 0;
 }
